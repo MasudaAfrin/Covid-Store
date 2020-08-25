ab7a65cf510578e61e047483ef52cb1395251471
MasudaAfrin <masudatuba@gmail.com>
2020-08-25 15:00:36 +0600

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(function(){
+    //sticky top
+    var windw = $(window);
+    windw.on("scroll",function(){
+        var scrolling = windw.scrollTop();
+        if(scrolling>0){
+            $('#navs').addClass('sticky');
+        }
+        else{
+            $('#navs').removeClass('sticky');
+        }
+    })
+
+    //filter button active showing
     var btnActive = $('.sortBtn button');
     btnActive.click(function(){
         $(this).addClass('active').siblings().removeClass('active');
     });
+    //lightbox 
     $('.store-items').magnificPopup({
         delegate: 'a',
         type: 'image',
--- js/product-filter.js
+++ js/product-filter.js
@@ -1,3 +1,4 @@
+
 let products = [
     {
         id: 1,
@@ -42,9 +43,28 @@ let products = [
         category: "sanitizer"
     }
 ]
+let showcartlist = [];
 const storeContainer = document.querySelector('.store-items');
 const filterbtn = document.querySelectorAll('.sortBtn button');
 const search = document.querySelector('#search-item');
+const shop = document.querySelector('.cart');
+const container = document.querySelector('.cart .allAdd');
+let clearAll = document.getElementById('cart-items-clear');
+let proceed = document.getElementById('checkout');
+console.log(clearAll);
+let totalQunatity = 0;
+let totalCost = 0;
+//showing cart
+const cartMainBtn = document.getElementById('cart-info');
+const cart = document.getElementById('cart');
+    //press cart option
+    cartMainBtn.addEventListener('click',function(){
+        cart.classList.toggle('show-cart');
+        window.scrollTo(0,0);
+        if(showcartlist.length == 0){
+            container.innerHTML = "No product to show";
+        }
+    });
 
 function check(){
     filterbtn.forEach(c =>{
@@ -57,6 +77,7 @@ function check(){
 //showing all products first when page loaded
 window.addEventListener('DOMContentLoaded',() =>{
     displayAllProducts(products);
+    
 });
 
 //Activity for filter button
@@ -103,6 +124,7 @@ search.addEventListener('keyup',(event)=>{
            
         }
         check();
+        
     }
     else{
         if(getValue === ""){
@@ -114,7 +136,7 @@ search.addEventListener('keyup',(event)=>{
 });
 
 //function for display product in store items section
- function displayAllProducts(itemall){
+function displayAllProducts(itemall){
     let displayProduct = itemall.map(function(item){
         
         return `
@@ -136,4 +158,185 @@ search.addEventListener('keyup',(event)=>{
     });
     displayProduct = displayProduct.join("");
     storeContainer.innerHTML = displayProduct;
+    addCartFunc();
  }
+
+ //add to cart functionality
+ function setItemCart(items){
+     let index;
+     if(showcartlist.length > 0){
+        showcartlist.filter(function(item){
+            if(items.title === item.title){
+                index = showcartlist.indexOf(item);
+            }
+        });
+        if(index == undefined){
+            showcartlist.push(items);
+        }
+        else{
+            showcartlist[index].qunatity +=1;
+        }
+    
+     }
+     else{
+         showcartlist.push(items);
+     }
+     //display inCart section
+     displayinCart(showcartlist);
+     showItemsinCart(showcartlist);
+  }
+
+//display in-cart function
+function displayinCart(){
+    //console.log(list);
+    //fill cost in navbar and cart div
+    let totalContainer = document.querySelector('.cart-info p .item-total');
+    let totalCart = document.querySelector('.cart-total-container #cart-total');
+    totalContainer.textContent = totalCost;
+    totalCart.textContent = totalCost;
+    //fill item in navbar
+    let itemContainer = document.querySelector('.cart-info p #item-count');
+    itemContainer.textContent = totalQunatity;
+    
+}
+function showItemsinCart(listItem){
+    var i=0;
+    var j=0;
+    var k=0;
+    let showinCart = listItem.map(det =>{
+        
+        return `
+        <div class="cart-item d-flex justify-content-between align-items-center my-1">
+            <img src="${det.image}" class="rounded-circle" id="item-img" alt="cart-img" width="50px" height="50px">
+            <!-- detail -->
+            <div class="cart-item-text text-center">
+                <p id="cart-item-title" class="mb-0 text-capitalize">${det.title}
+                </p>
+                <span>&nbsp &#x9f3</span>
+                <span class="cart-item-price mb-0" id="cart-item-price">${det.price}</span>
+            </div>
+            <!--Quantity-->
+            <div class="cart-item-quantity d-flex justify-content-between">
+                <span class="mb-0 mr-2"><i class="fas fa-angle-left" onclick = "decOne(${j++})"></i></span>
+                <p class="font-weight-bold" id="quan">${det.qunatity}</p>
+                <span class="mb-0 ml-2"><i class="fas fa-angle-right" onclick = "incOne(${k++})"></i></span>
+            </div>
+            <span class="cart-item-remove" id="cart=item-remove"><i class="fas fa-trash-alt" onclick="delItem(${i++})"></i></span>
+         </div>
+        `
+    });
+    showinCart = showinCart.join("");
+    container.innerHTML = showinCart;
+}
+//increase
+function incOne(rec){
+    let number = showcartlist[rec].qunatity;
+    number += 1;
+    //nav-info update
+    totalQunatity +=1;
+    let p = showcartlist[rec].price;
+    totalCost += p;
+    displayinCart();
+    //incart Update
+    showcartlist[rec].qunatity = number;
+    showItemsinCart(showcartlist);
+}
+//decrease
+function decOne(rec){
+    //console.log(rec);
+    let number = showcartlist[rec].qunatity;
+    if(number>1){
+        number -= 1;
+        totalQunatity -=1;
+        let p = showcartlist[rec].price;
+        totalCost -= p;
+        displayinCart();
+        //incart Update
+        showcartlist[rec].qunatity = number;
+        showItemsinCart(showcartlist);
+    }
+    else{
+        alert("You must keep 1 product");
+    }
+    
+}
+//delete
+function delItem(take){
+    let number = showcartlist[take].qunatity;
+    let price = showcartlist[take].price;
+    totalQunatity-=number;
+    totalCost-=(number*price);
+    displayinCart();
+    showcartlist.splice(take,1);
+    if(showcartlist.length == 0){
+        container.innerHTML = "No product to show";
+    }
+    else{
+        showItemsinCart(showcartlist);
+    }
+}
+ //added when clicked
+function addCartFunc(){
+    const a = document.querySelectorAll('.store-item-icon');
+    
+    //event for add cart request
+    a.forEach((addCart) => {
+        addCart.addEventListener('click',function(event){
+            //console.log(event.target.parentElement)
+            let cartItems = {};
+          if(event.target.parentElement.classList.contains("store-item-icon")){
+            //   taking image here
+              let imgPath = event.target.parentElement.previousElementSibling.firstElementChild.src;
+              //slice the path before img folder
+              let position = imgPath.indexOf("images"); 
+              let image = imgPath.slice(position);
+              cartItems.image = image;
+
+              //taking title of product here
+              let getName = event.target.parentElement.parentElement.nextElementSibling.firstElementChild.firstElementChild.textContent;
+              cartItems.title = getName;
+
+              //taking price
+              let price = event.target.parentElement.parentElement.nextElementSibling.firstElementChild.
+              lastElementChild.lastElementChild.textContent;
+              price = parseInt(price);
+              cartItems.price = price;
+              //take quantity
+              cartItems.qunatity = 1;
+             //totalqunatity
+              totalQunatity +=1;
+            //total cost
+              totalCost += price;
+              //set to the showcartList
+              getName = getName.toUpperCase();
+              alert(getName+" added to the cart succesfully!")
+              setItemCart(cartItems);
+              
+          }
+        })
+    });
+ }
+//procced btn
+function proc(){
+    proceed.addEventListener('click',function(){
+        alert("Thank you for your order!");
+        showcartlist.length = 0;
+        container.innerHTML = "No product to show";
+        totalCost =0;
+        totalQunatity=0;
+        displayinCart();
+    });
+}
+//function clear
+function removeall(){
+    clearAll.addEventListener('click',function(){
+        alert("Your products are removed");
+        showcartlist.length = 0;
+        totalCost =0;
+        totalQunatity=0;
+        displayinCart();
+        container.innerHTML = "No product to show";
+    });
+}
+proc();
+removeall();
